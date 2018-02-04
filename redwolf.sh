016da6f4b4999f344b64f73bb3a2b47c9c57d028
Daniel Sikorski <dadi.sikorski@gmail.com>
2018-02-04 21:15:42 +0100

    rw: improve redwolf.sh
    remove things we don't need, simplify and rename

--- redwolf.sh
+++ redwolf.sh
@@ -3,18 +3,17 @@
 echo "-----------------Making RedWolf-----------------"
 
 echo "-- Setting up Environment Variables"
-RW_BUILD=025 #Set RedWolf Version
+if [ -z "$TW_DEVICE_VERSION" ]; then
+RW_BUILD=Unofficial
+else
+RW_BUILD=$TW_DEVICE_VERSION
+fi
 RW_VENDOR=vendor/redwolf
 RW_WORK=$OUT/RW_AIK
 RW_2GB=$OUT/RW_2GB
-BUILD_DATE=$(date +"%Y%m%d")
 RW_DEVICE=$(cut -d'_' -f2 <<<$TARGET_PRODUCT)
 
-if [ -z "$RW_TYPE" ]; then # Set Build Type in Device Tree
-  RW_TYPE="UNOFFICIAL"
-fi
-
-RW_OUT_NAME=RedWolf-$RW_BUILD-$RW_DEVICE-$BUILD_DATE-$RW_TYPE
+RW_OUT_NAME=RedWolf-$RW_BUILD-$RW_DEVICE
 
 if [ -d "$RW_WORK" ]; then
   echo "-- AIK Found in OUT"
@@ -38,7 +37,7 @@ bash "$RW_WORK/repackimg.sh" > /dev/null 2>&1
 cp "$RW_WORK/image-new.img" "$OUT/$RW_OUT_NAME.img"
 cd "$OUT" && md5sum "$RW_OUT_NAME.img" > "$RW_OUT_NAME.img.md5" && cd - > /dev/null 2>&1
 
-if [ "$DEVICE_HAS_2GB_VARIANT" = true ] ; then
+if [ "$DEVICE_HAS_2GB_VARIANT" = "true" ]; then
   echo '-- 2GB Variant Found'
   rm -rf "$RW_2GB"
   mkdir "$RW_2GB"
@@ -49,8 +48,8 @@ if [ "$DEVICE_HAS_2GB_VARIANT" = true ] ; then
   cp "$OUT/recovery.img" "$RW_2GB/tools"
   sed -i -- "s/devicenamehere/${RW_DEVICE}/g" "$RW_2GB/META-INF/com/google/android/update-binary"
   echo '-- Compressing Files to ZIP'
-  cd "$RW_2GB" && zip -r "$OUT/$RW_OUT_NAME-2GB_VARIANT.zip" ./* > /dev/null 2>&1 && cd - > /dev/null 2>&1
-  cd "$OUT" && md5sum "$RW_OUT_NAME-2GB_VARIANT.zip" > "$RW_OUT_NAME-2GB_VARIANT.zip.md5" && cd - > /dev/null 2>&1
+  cd "$RW_2GB" && zip -r "$OUT/$RW_OUT_NAME-2GB_RAM.zip" ./* > /dev/null 2>&1 && cd - > /dev/null 2>&1
+  cd "$OUT" && md5sum "$RW_OUT_NAME-2GB_RAM.zip" > "$RW_OUT_NAME-2GB_RAM.zip.md5" && cd - > /dev/null 2>&1
 fi
 
 echo "------------Finished Making RedWolf-------------"
