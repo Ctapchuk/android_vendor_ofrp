75318c0d31f84bc01f1ee0fc3ede6b4e9908274d
Devil7 <dineshthangavel47@gmail.com>
2018-02-12 16:39:29 +0000

    Use mkbootimg instead of AIK

--- redwolf.sh
+++ redwolf.sh
@@ -16,25 +16,18 @@ RW_DEVICE=$(cut -d'_' -f2 <<<$TARGET_PRODUCT)
 RW_OUT_NAME=RedWolf-$RW_BUILD-$RW_DEVICE
 
 if [ -d "$RW_WORK" ]; then
-  echo "-- AIK Found in OUT"
-  rm "$RW_WORK/recovery.img"
-  bash "$RW_WORK/cleanup.sh" > /dev/null 2>&1
-else
-  echo "-- Copying AIK to OUT"
-  cp -R "$RW_VENDOR/AIK" "$RW_WORK"
+  echo "-- Working Folder Found in OUT. Removing it."
+  rm -rf "$RW_WORK"
 fi
 
-cp "$OUT/recovery.img" "$RW_WORK"
-
 echo "-- Unpacking Recovery"
-bash "$RW_WORK/unpackimg.sh" "--nosudo" "$RW_WORK/recovery.img" > /dev/null 2>&1
+bash "$RW_VENDOR/tools/mkboot" "$OUT/recovery.img" "$RW_WORK" > /dev/null 2>&1
 
 echo "-- Including WolfShit"
 cp -R "$RW_VENDOR/prebuilt/WolfShit" "$RW_WORK/ramdisk/WolfShit"
 
 echo "-- Repacking and Copying Recovery"
-bash "$RW_WORK/repackimg.sh" > /dev/null 2>&1
-cp "$RW_WORK/image-new.img" "$OUT/$RW_OUT_NAME.img"
+bash "$RW_VENDOR/tools/mkboot" "$RW_WORK" "$OUT/$RW_OUT_NAME.img" > /dev/null 2>&1
 cd "$OUT" && md5sum "$RW_OUT_NAME.img" > "$RW_OUT_NAME.img.md5" && cd - > /dev/null 2>&1
 
 if [ "$DEVICE_HAS_2GB_VARIANT" = "true" ]; then
